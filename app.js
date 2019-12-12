540029e95f8ead04f0331b7836fcc9515b7e1289
Prajakta244 <prajakta2498shinde@gmail.com>
2019-12-12 18:17:51 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,56 @@
+var express = require("express")
+var app = express()
+var bodyparser = require("body-parser")
+var mongoose      = require("mongoose")
+app.use(bodyparser.urlencoded({extended:true}))
+app.use(express.static("public"))
+
+mongoose.set('useUnifiedTopology', true);
+mongoose.set('useCreateIndex', true)
+mongoose.connect("mongodb://localhost/movie_detail",{ useNewUrlParser: true })
+
+var movieSchema = new mongoose.Schema({
+	name:String,
+	image:String,
+	summary:String
+})
+
+var Movie = mongoose.model("Movie",movieSchema)
+
+app.set("view engine", "ejs")
+
+app.get("/",function(req,res){
+	res.render("landing")
+})
+
+app.get("/movie", function(req,res){
+	Movie.find({}, function(err,allMovies){
+		if(err){
+			console.log(err)
+		}
+		else{
+			res.render("movie",{movies : allMovies})	
+		}
+	})
+})
+app.post("/movie",function(req,res){
+	var name = req.body.name;
+	var image = req.body.image;
+	var summary = req.body.summary;
+	var newMovie = {name : name, image : image, summary : summary}
+	Movie.create(newMovie, function(err, recentMovie){
+		if(err){
+			console.log(err)
+		}
+		else{
+			res.redirect("/movie")
+		}
+	})
+})
+app.get("/movie/new", function(req,res){
+	res.render("new")
+})
+
+app.listen(8000,()=>{
+	console.log("App Begins")
+})
