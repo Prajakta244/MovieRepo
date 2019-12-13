1772022df48417df3a53ae61266e8b63b5ef39f5
Prajakta244 <58784411+Prajakta244@users.noreply.github.com>
2019-12-13 10:55:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-var express = require("express")
-var app = express()
+var express    = require("express")
+var app        = express()
 var bodyparser = require("body-parser")
-var mongoose      = require("mongoose")
+var mongoose   = require("mongoose")
+
 app.use(bodyparser.urlencoded({extended:true}))
 app.use(express.static("public"))
 
@@ -9,12 +10,14 @@ mongoose.set('useUnifiedTopology', true);
 mongoose.set('useCreateIndex', true)
 mongoose.connect("mongodb://localhost/movie_detail",{ useNewUrlParser: true })
 
+//Schema Definition
 var movieSchema = new mongoose.Schema({
 	name:String,
 	image:String,
 	summary:String
 })
 
+//Creating New Model
 var Movie = mongoose.model("Movie",movieSchema)
 
 app.set("view engine", "ejs")
@@ -23,6 +26,7 @@ app.get("/",function(req,res){
 	res.render("landing")
 })
 
+//Fetching all movies from db
 app.get("/movie", function(req,res){
 	Movie.find({}, function(err,allMovies){
 		if(err){
@@ -33,6 +37,8 @@ app.get("/movie", function(req,res){
 		}
 	})
 })
+
+//Inserting movie into db
 app.post("/movie",function(req,res){
 	var name = req.body.name;
 	var image = req.body.image;
